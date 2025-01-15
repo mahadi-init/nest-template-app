d84d384f0531dcaad36f92c77057b38f94d069b5
mahadi-init <init.mahadi@gmail.com>
2025-01-15 14:17:33 +0600

    change static port to env port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/routes/app/app.module';
+import config from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.useGlobalInterceptors(new LoggingInterceptor());  // logging
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.port ?? 3000);
 }
 bootstrap();
